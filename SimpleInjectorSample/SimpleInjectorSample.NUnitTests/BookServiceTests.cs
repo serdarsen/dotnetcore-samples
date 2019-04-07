ff86165cd8b30f96e75643d18e6f379f766285d6
serdarsen <serdarsendev@gmail.com>
2019-04-07 18:42:45 +0300

    SimpleInjectorSample.NUnitTests added

---
+++ SimpleInjectorSample/SimpleInjectorSample.NUnitTests/BookServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace SimpleInjectorSample.NUnitTests
+{
+    public class BookServiceTests : ServiceTests
+    {
+        [SetUp]
+        public void run_before_every_test()
+        {
+            
+        }
+
+        [Test]
+        public void BookService_Save()
+        {
+
+        }
+    }
+}
---
+++ SimpleInjectorSample/SimpleInjectorSample.NUnitTests/ServiceTests.cs
@@ -0,0 +1,32 @@
+using Moq;
+using NUnit.Framework;
+using SimpleInjector;
+
+namespace SimpleInjectorSample.NUnitTests
+{
+    public class ServiceTests
+    {
+        // todo:
+        //public static readonly Container container;
+
+        //public Mock<IBooksRepository> MockIBooksRepository => new Mock<IBooksRepository>();
+        //public IBooksRepository BooksRepository => MockIBooksRepository.Object;
+
+        //public Mock<ILogger> MockILogger => new Mock<ILogger>();
+        //public ILogger Logger => MockILogger.Object;
+
+        //static ServiceTests()
+        //{
+        //    // 1. Create a new Simple Injector container
+        //    container = new Container();
+
+        //    // 2. Configure the container (register)
+        //    container.Register<IBooksRepository, BooksRepository>();
+        //    container.Register<ILogger, Logger>();
+        //    container.Register<BookService>();
+
+        //    // 3. Verify your configuration
+        //    container.Verify();
+        //}
+    }
+}
