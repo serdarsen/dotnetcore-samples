a16b5347a779c4bfec75d4aad04905aaf3623516
serdarsen <serdarsendev@gmail.com>
2019-04-09 23:09:53 +0300

    CastleWindsorSample and CastleAopInterception added

---
+++ CastleWindsorSample/CastleWindsorSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CastleWindsorSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SimpleInjectorSample/SimpleInjectorSample/IBookService.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleInjectorSample
+{
+    public interface IBookService
+    {
+        Book GetById(int id);
+        void Save(Book book);
+    }
+}
